65f8f672ca09235cce624f354c413cdd6d766fea
EvgeniRay <titkov.evgen@gmail.com>
2024-05-22 14:04:17 +0100

    Virtualization - part 1 - end

--- 5-virtualisation/5-1-skeleton/end/virtual-list.js
+++ 5-virtualisation/5-1-skeleton/end/virtual-list.js
@@ -1,10 +1,3 @@
-/**
- * Part 1 - Building skeleton
- *
- * @todo
- * 1. Update toHTML to return the structure described in goal.png
- * 2. Run index.html to see if we render it
- */
 export class VirtualList {
     /**
      * @param root
@@ -14,35 +7,39 @@ export class VirtualList {
         this.props = {...props};
         this.root = root;
     }
+
     /**
      * Returns an HTML Representation of the component, should have the following structure:
      * #container>
-     *    #top-observer+
+     *    .top-observer+
      *    #virtual-list+
      *    #bottom-observer
      * @returns {string}
      */
     toHTML() {
-        /**
-         * Part 1 - App Skeleton
-         *
-         * @todo
-         * Create container div which will include:
-         *   - top-observer
-         *   - virtual-list
-         *   - bottom-observer
-         */
-        return ``;
+        return `<div id="container">
+        <div id="top-observer">Top Observer</div>
+        <main id="virtual-list"></main>
+        <div id="bottom-observer">Bottom Observer</div>
+        </div>`.trim();
     }
+
     /**
+     * Registers Events / Observers, this function is run after initial render
      * @returns void
      */
     #effect() {}
+
     /**
+     * Renders the content to the provided root container and runs
+     * any additional side effects
      * @returns void
      */
     render() {
         this.root.innerHTML = this.toHTML();
         this.#effect()
     }
+
+
 }
+
